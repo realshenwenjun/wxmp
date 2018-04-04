a173aebb31112ea63d5dd2a80c7fbbce24ecc45f
shenwenjun3d@163.com <shenwenjun3d@163.com>
2018-04-04 08:58:04 +0800

    first commit

---
+++ src/main/java/com/wxmp/wxcms/mapper/ImgResourceDao.java
@@ -0,0 +1,37 @@
+package com.wxmp.wxcms.mapper;
+
+import com.wxmp.wxcms.domain.ImgResource;
+
+/** 
+ * @title : 
+ * @description : 
+ * @projectname : wxmp
+ * @classname : ImgResourceDao
+ * @version 1.0
+ * @author : hermit
+ * @createtime : 2017年6月13日 下午5:06:35
+*/
+
+public interface ImgResourceDao {
+	/**
+	 * 获取图片信息
+	 * @param id
+	 * @return
+	 */
+	public ImgResource getImgById(String id);
+	
+	/**
+	 * 创建资源
+	 * @param img
+	 * @return
+	 */
+	public void add(ImgResource img);
+	
+	/**
+	 * 删除中间表记录
+	 * @param otherId
+	 * @return
+	 */
+	public void deleteByMediaId(String otherId);
+	
+}   
